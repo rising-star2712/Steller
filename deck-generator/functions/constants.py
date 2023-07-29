f901a205198a95528cda7517c46203f23248ca49
Prince Thakkar <princethakkar27@gmail.com>
2023-07-29 17:22:46 +0530

    first commit
    commit files

---
+++ deck-generator/functions/constants.py
@@ -0,0 +1,21 @@
+import torch
+
+
+BACKGROUNDS = {
+    "style1.css": ["./templates/backgrounds/bg_img_style11.png","./templates/backgrounds/bg_img_style12.png","./templates/backgrounds/bg_img_style13.png"],
+    "style2.css": ["./templates/backgrounds/bg_img_style21.png","./templates/backgrounds/bg_img_style22.png","./templates/backgrounds/bg_img_style23.png"],
+    "style3.css": ["./templates/backgrounds/bg_img_style31.png","./templates/backgrounds/bg_img_style32.png","./templates/backgrounds/bg_img_style33.png"]
+}
+
+# "./templates/backgrounds/bg_img_style10.png","./templates/backgrounds/bg_img_style20.png","./templates/backgrounds/bg_img_style30.png",
+
+NEG_PROMPT = """
+"UnrealisticDream, FastNegativeEmbedding,worst quality, low quality:1.4, blurry:1.2,blurry face,
+blurry eyes,greyscale, monochrome:1.1, 3D face, cropped, text, jpeg artifacts, watermark, username,
+blurry, artist name, watermark, title, multiple view, , plump, fat, muscular female, strabismus, out of frame,
+ugly, extra limbs, bad anatomy,p gross proportions, malformed limbs, missing arms, missing legs,
+extra arms, extra legs, mutated hands, fused fingers, distorted face, too many fingers,
+long neck"
+"""
+
+DEVICE = "cuda" if torch.cuda.is_available else "cpu"
---
+++ deck-generator/main.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Import requirements
+
+# In[ ]:
+
+
+from functions.main_functions import *
+
+
+# In[ ]:
+
+
+#@markdown ###### Enter a topic:
+topic="Organic Farming" #@param {type:"string"}
+#@markdown ###### Select a style:
+Style = "Style 2" #@param ["Style 1","Style 2","Style 3"] {type:"string"}
+#@markdown ###### Select a number of slides
+Number_of_Slides=5 #@param {type:"slider", min:2, max:7, step:1}
+#@markdown ###### Select the image source
+image_source="Pexel"#@param ["Pexel","Unsplash","Stable Diffusion"] {type:"string"}
+#@markdown ###### Select a Stable diffusion model if image source is "Stable diffusion"
+sd_model= "Lykon/DreamShaper" #@param {type:"string"}
+#@markdown ###### Select an Openai Model:
+engine="gpt-3.5-turbo" #@param ["gpt-4-32k-0613","gpt-4-32k","gpt-4-0613","gpt-4","gpt-3.5-turbo-16k-0613","gpt-3.5-turbo-0613","gpt-3.5-turbo-16k","gpt-3.5-turbo","text-ada-001","text-babbage-001","text-curie-001","text-davinci-002","text-davinci-003"]{type:"string"}
+#@markdown ###### Enter your OpenAi api key here:
+api_key = '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'#@param {type:"string"}
+
+text_models=["text-ada-001","text-babbage-001","text-curie-001","text-davinci-002","text-davinci-003"]
+chat_models=["gpt-3.5-turbo-16k-0613","gpt-3.5-turbo-0613","gpt-3.5-turbo-16k","gpt-3.5-turbo"]
+styles={"Style 1":"style1.css","Style 2":"style2.css","Style 3":"style3.css"}
+type_of_slides=["para-1", "para-2","img", "points"]
+
+use_chat = False if engine in text_models else True
+
+style=styles[Style]
+slides = ["slide-1:title"]
+
+nums=Number_of_Slides-(Number_of_Slides//2)
+slides.extend([f"slide-{i+2}:{type_of_slides[0:2][int(random.randint(0,1))]}" for i in range(nums)])
+slides.extend([f"slide-{i+nums+2}:{type_of_slides[2:4][int(random.randint(0,1))]}" for i in range(Number_of_Slides-(nums)-1)])
+
+
+# In[ ]:
+
+
+bg_image=image_to_base64(get_background(style))
+
+content = None
+while content == None :
+    content = generate_content(slides,topic,engine,api_key,use_chat)
+    if content != None: 
+        break
+    else:
+        continue
+
+  
+
+
+# ### Make sure that the `content` is not `None`.
+# #### At times openai api returns an error due to various reasons,`re-run` the above cell till `content` is not `None`.
+
+# In[ ]:
+
+
+#print(content)
+
+
+# In[ ]:
+
+
+html=generate_template(style, slides,content,bg_image,image_source).render()
+
+
+# ##### This cell may return an error but it can be ignore, the pdf can be seen in the left panel
+
+# In[ ]:
+
+print(html)
+#render_pdfkit(html,topic.replace(" ","-")+".pdf")
+
+
+# In[ ]:
+
+
+
+
